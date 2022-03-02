bc6d40e32066b5f251db45410fcd25e3dba01de4
Dawson <dawsonreschke@icloud.com>
2022-03-02 06:38:32 -0800

    displaying an untested example of how to use the  library

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,41 @@
+const mic = require('mic')
+const fs = require('fs') 
 const express = require('express');
-
-const emojis = require('./emojis');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'API - 👋🌎🌍🌏'
-  });
-});
+const micInstance = mic({
+  rate:16000,
+  channels:'1',
+  debug:true,
+  exitOnSilence:10,
+})
 
-router.use('/emojis', emojis);
+
+// should look something like this: 
+
+router.get('/',(req,res,next)=>{
+  const micInput = micInstance.getAudioStream()
+  micInput.pipe(res);
+  micInput.on('processExitComplete',()=>{
+    res.send(); 
+  })
+  micInstance.start();
+})
+
+// Example: stream audio file
+// router.get('/', (req, res) => {
+//   res.header({
+//     "Content-Type":"audio/mpeg"
+//   })
+//   let micInputStream = fs.createReadStream('/Users/dawson/workspace/homeAutomation/Server-hosted-audio-stream/20220302 053008.m4a')
+//     micInputStream.on('open',()=>{
+//       micInputStream.pipe(res)
+//     })
+//     micInputStream.on('end',()=>{
+//       res.send()
+//     })
+// });
+
 
 module.exports = router;
--- src/app.js
+++ src/app.js
@@ -17,11 +17,11 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
+    message: 'you made it to the slash route'
   });
 });
 
-app.use('/api/v1', api);
+app.use('/api', api);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
